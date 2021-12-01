62681cce24e48aaa5f203e23b759e45c00d569fe
Lanelle Runcorn <lruncorn@oa-l3.msk.21-school.ru>
2021-12-01 17:50:20 +0300

    pidorsky cgi

---
+++ YoupiBanane/hello.php
@@ -0,0 +1,9 @@
+#!/usr/bin/php
+<html>
+  <head>
+    <title>Hello, World!</title>
+  </head>
+  <body>
+    <p><?php echo "Hello, PHP World!"; ?></p>
+  </body>
+</html>
--- srcs/Cgi/Cgi.cpp
+++ srcs/Cgi/Cgi.cpp
@@ -41,63 +41,190 @@ void Cgi::env_to_array()
     env_ready = env_arr;
 }
 
+// void Cgi::startCgi(){
+//     int         save0;
+//     int         save1;
+//     std::string res;
+//     pid_t       pid;
+//     std::string cgi = this->conf.cgi_pass;
+//     int ret = 1;
+
+//     env_to_array();
+//     std::string s;
+//     s = this->conf.cgi_pass;
+
+//     char **args;
+//     args[0] = (char *)s.c_str();
+//     args[1] = (char *)req.path.c_str();
+//     std::cout << args[1] << std::endl;
+//     args[2] = NULL;
+
+//     FILE *fin = tmpfile();
+//     FILE *fout = tmpfile();
+//     long fdin = fileno(fin);
+//     long fdout = fileno(fout);
+//     save0 = dup(0);
+//     save1 = dup(1);
+
+//     pid = fork();
+
+//     if (pid == -1){
+//         body = "Status: 500\r\n\r\n";
+//         return;
+//     }
+//     else if (pid == 0){
+//         dup2(fdin, 0);
+//         dup2(fdout, 1);
+//         execve(cgi.c_str(), NULL, env_ready);
+//         std::cerr << "Execve error" << std::endl;
+//         write(1, "Status: 500\r\n\r\n", 16);
+//     }
+  
+// }
+
+    // for(size_t i = 0; env_ready[i]; i++)
+    //     delete [] env_ready[i];
+    // // delete[] env_ready;
+
+    // if (!pid)
+    //     exit(0);
+    // body = res;
+
+    // int fd;
+    // FILE *file = tmpfile();
+    // fd = fileno(file);
+    // int tubes[2];
+    // pipe(tubes);
+    // pid_t pid;
+
+    // char **args;
+    // args[0] = (char *)res.Conf.cgi_pass.c_str();
+    // args[1] = (char *)req.path.c_str();
+    // args[2] = NULL;
+
+    // env_to_array();
+
+    // if ((pid = fork() == 0)){
+    //     close(tubes[1]);
+    //     dup2(tubes[0], 0);
+    //     execve(args[0], args, env_ready);
+    // }
+    // else{
+
+    // }
+
 void Cgi::startCgi(){
-    int         save0;
-    int         save1;
-    std::string res;
-    pid_t       pid;
-    std::string cgi = this->conf.cgi_pass;
-    int ret = 1;
+    pid_t pid;
+    int fdout = open("file", O_CREAT | O_TRUNC | O_WRONLY, 0777);
+	// if (head->fdin != -1)
+	// 	dup2(head->fdin, 0);
+	// if (head->fdout != -1)
+	// 	dup2(head->fdout, 1);
+	// if (is_builtin(head->args[0]) != -1)
+	// 	return (do_builtin(head));
 
-    env_to_array();
+    char **args = (char **)malloc(sizeof(char *) * 3);
+    args[0] = (char *)res.Conf.cgi_pass.c_str();
+    args[1] = (char *)req.path.c_str();
+    args[2] = NULL;
+    int fd = dup(1);
 
-    FILE *fin = tmpfile();
-    FILE *fout = tmpfile();
-    long fdin = fileno(fin);
-    long fdout = fileno(fout);
-    save0 = dup(0);
-    save1 = dup(1);
-
-    pid = fork();
-
-    if (pid == -1){
-        body = "Status: 500\r\n\r\n";
-        return;
+	pid = fork();
+	dup2(fdout, 1);
+	if (pid == 0){
+        close(1);
+        system((char *)(this->conf.cgi_pass + " " + req.path + " > file").c_str());
+        // execve(args[0], args, env_ready);
+        exit(-1);
     }
-    else if (pid == 0){
-        dup2(fdin, 0);
-        dup2(fdout, 1);
-        execve(cgi.c_str(), NULL, env_ready);
-        std::cerr << "Execve error" << std::endl;
-        write(1, "Status: 500\r\n\r\n", 16);
-    }
-    else{
-        char buf[10] = {0};
-        waitpid(-1, NULL, 0);
-        lseek(fdout, 0, SEEK_SET);
-
-        ret = 1;
-        while (ret > 0){
-            memset(buf, 0, 10);
-            ret = read(fdout, buf, 10);
-            res += buf;
-        }
-    }
-
-    dup2(save0, 0);
-    dup2(save1, 1);
-    fclose(fin);
-    fclose(fout);
-    close(fdin);
+	else
+		waitpid(pid, 0, 0);
+    dup2(fd, 1);
     close(fdout);
-    close(save0);
-    close(save1);
-
-    for(size_t i = 0; env_ready[i]; i++)
-        delete [] env_ready[i];
-    // delete[] env_ready;
-
-    if (!pid)
-        exit(0);
-    body = res;
+    std::string q = "file";
+    body = readFile(q);
 }
+ 
+
+// void Cgi::startCgi(){
+//     pid_t		pid;
+// 	int			saveStdin;
+// 	int			saveStdout;
+// 	char		**env;
+// 	std::string	newBody;
+
+// 	this->env_to_array();
+
+// 	// SAVING STDIN AND STDOUT IN ORDER TO TURN THEM BACK TO NORMAL LATER
+// 	saveStdin = dup(STDIN_FILENO);
+// 	saveStdout = dup(STDOUT_FILENO);
+
+// 	FILE	*fIn = tmpfile();
+// 	FILE	*fOut = tmpfile();
+// 	long	fdIn = fileno(fIn);
+// 	long	fdOut = fileno(fOut);
+// 	int		ret = 1;
+
+// 	write(fdIn, req.body.c_str(), req.body.size());
+// 	lseek(fdIn, 0, SEEK_SET);
+
+//         char **args = (char **)malloc(sizeof(char *) * 3);
+//     args[0] = (char *)res.Conf.cgi_pass.c_str();
+//     // args[1] = (char *)req.path.c_str();
+//     args[1] = NULL;
+//     args[2] = NULL;
+
+// 	pid = fork();
+
+// 	if (pid == -1)
+// 	{
+// 		std::cerr <<  "Fork crashed."  << std::endl;
+// 		newBody = "Status: 500\r\n\r\n";
+// 	}
+// 	else if (!pid)
+// 	{
+// 		char * const * nll = NULL;
+
+// 		dup2(fdIn, STDIN_FILENO);
+// 		dup2(fdOut, STDOUT_FILENO);
+
+//         system((char *)(this->conf.cgi_pass + " " + req.path).c_str());
+// 		// execve(this->conf.cgi_pass.c_str(), NULL, env);
+// 		// std::cerr <<  "Execve crashed."  << std::endl;
+// 		// write(STDOUT_FILENO, "Status: 500\r\n\r\n", 15);
+// 	}
+// 	else
+// 	{
+// 		char	buffer[1000] = {0};
+
+// 		waitpid(-1, NULL, 0);
+// 		lseek(fdOut, 0, SEEK_SET);
+
+// 		ret = 1;
+// 		while (ret > 0)
+// 		{
+// 			memset(buffer, 0, 1000);
+// 			ret = read(fdOut, buffer, 1000 - 1);
+// 			newBody += buffer;
+// 		}
+// 	}
+
+// 	dup2(saveStdin, STDIN_FILENO);
+// 	dup2(saveStdout, STDOUT_FILENO);
+// 	fclose(fIn);
+// 	fclose(fOut);
+// 	close(fdIn);
+// 	close(fdOut);
+// 	close(saveStdin);
+// 	close(saveStdout);
+
+// 	// for (size_t i = 0; env[i]; i++)
+// 	// 	delete[] env[i];
+// 	// // delete[] env;
+
+// 	if (!pid)
+// 		exit(0);
+
+// 	std::cout << newBody << std::endl;
+//     this->body = newBody;
+// }
